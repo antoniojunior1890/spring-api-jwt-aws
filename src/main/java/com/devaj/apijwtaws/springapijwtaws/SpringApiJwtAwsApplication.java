43c9bd570e82b3282063fea6ed236e0709b2a51b
antoniojunior1890 <antonio.junior1890@gmail.com>
2019-08-26 21:58:16 -0300

    primeiro up

---
+++ src/main/java/com/devaj/apijwtaws/springapijwtaws/SpringApiJwtAwsApplication.java
@@ -0,0 +1,13 @@
+package com.devaj.apijwtaws.springapijwtaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiJwtAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiJwtAwsApplication.class, args);
+	}
+
+}
