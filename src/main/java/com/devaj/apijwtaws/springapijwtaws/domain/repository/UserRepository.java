ce2c73bfa06815a75bc8a87d3b13bcd9d65bcb0a
antoniojunior1890 <antonio.junior1890@gmail.com>
2019-08-26 23:35:00 -0300

    camada repository

---
+++ src/main/java/com/devaj/apijwtaws/springapijwtaws/domain/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.devaj.apijwtaws.springapijwtaws.domain.repository;
+
+import com.devaj.apijwtaws.springapijwtaws.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u from User u where u.email = :email and u.password = :password")
+    Optional<User> login(@Param("email")String email, @Param("password") String password);
+}
