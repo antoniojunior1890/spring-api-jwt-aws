877ee70e75ac141cb813ca162f130e738d7b83f5
antoniojunior1890 <antonio.junior1890@gmail.com>
2019-08-31 00:20:10 -0300

    conf POM para gerar war e class principal

--- src/main/java/com/devaj/apijwtaws/springapijwtaws/SpringApiJwtAwsApplication.java
+++ src/main/java/com/devaj/apijwtaws/springapijwtaws/SpringApiJwtAwsApplication.java
@@ -2,9 +2,10 @@ package com.devaj.apijwtaws.springapijwtaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringApiJwtAwsApplication {
+public class SpringApiJwtAwsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiJwtAwsApplication.class, args);
