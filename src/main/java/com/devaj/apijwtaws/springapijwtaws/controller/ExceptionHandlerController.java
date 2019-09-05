a738d83d2ac42bbe410d1748357a7ea83fee7f18
antoniojunior1890 <antonio.junior1890@gmail.com>
2019-09-05 17:10:22 -0300

    login pronto e exceção de login

--- src/main/java/com/devaj/apijwtaws/springapijwtaws/controller/ExceptionHandlerController.java
+++ src/main/java/com/devaj/apijwtaws/springapijwtaws/controller/ExceptionHandlerController.java
@@ -6,6 +6,7 @@ import com.devaj.apijwtaws.springapijwtaws.exception.NotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -24,6 +25,13 @@ public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
         ApiError apiError = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiError> handleBadCredentialsException(BadCredentialsException ex){
+        ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), new Date());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(apiError);
     }
 
     @Override
